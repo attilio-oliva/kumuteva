05481b691155c2d2cf6813db4f45c27c6d913ea1
Attilio Oliva <attilio.oliva@protonmail.com>
2025-01-15 16:27:10 +0100

    fix: vcluster api server connection

--- tests/obj-isolation/obj-isolation.sh
+++ tests/obj-isolation/obj-isolation.sh
@@ -1,20 +1,30 @@
-vcluster create tenant-1 --namespace tenant1 --values ../../solution_config/vcluster.yaml
-vcluster create tenant-2 --namespace tenant2 --values ../../solution_config/vcluster.yaml
+vcluster create tenant1 --namespace tenant1 --values ../../solution_config/vcluster.yaml
+vcluster create tenant2 --namespace tenant2 --values ../../solution_config/vcluster.yaml
 
-# Deploy nginx application in tenant-1
-kubectl apply -f nginx-deployment.yaml -n tenant1
+# Save the kubeconfig of tenant1 and tenant2
+vcluster connect tenant1 -n tenant1 --insecure --print > kubeconfig_tenant1.yaml
+vcluster connect tenant2 -n tenant2 --insecure --print > kubeconfig_tenant2.yaml
 
-# Check if the nginx pod is running in tenant-1
-kubectl get pods -n tenant1 --context vcluster_tenant-1_tenant1_kind-vcluster
+# Deploy nginx application in tenant1
+kubectl create ns tenant1-apps --kubeconfig kubeconfig_tenant1.yaml
+kubectl apply -f nginx-deployment.yaml -n tenant1-apps --kubeconfig kubeconfig_tenant1.yaml
 
-# Check if user tenant-2 can access tenant-1
-kubectl get pods -n tenant1 --context vcluster_tenant-2_tenant2_kind-vcluster
+# Check if the nginx pod is running in tenant1
+kubectl get pods -n tenant1-apps --kubeconfig kubeconfig_tenant1.yaml
 
-# Deploy nginx application in tenant-2
-kubectl apply -f nginx-deployment.yaml -n tenant2
+# Check if user tenant2 can access tenant1
+kubectl get pods -n tenant1-apps --kubeconfig kubeconfig_tenant2.yaml
 
-# Check if the nginx pod is running in tenant-2
-kubectl get pods -n tenant2 --context vcluster_tenant-2_tenant2_kind-vcluster
+# Deploy nginx application in tenant2
+kubectl create ns tenant2-apps --kubeconfig kubeconfig_tenant2.yaml
+kubectl apply -f nginx-deployment.yaml -n tenant2-apps --kubeconfig kubeconfig_tenant2.yaml
 
-# Check if user tenant-1 can access tenant-2
-kubectl get pods -n tenant2 --context vcluster_tenant-1_tenant1_kind-vcluster
+# Check if the nginx pod is running in tenant2
+kubectl get pods -n tenant2-apps --kubeconfig kubeconfig_tenant2.yaml
+
+# Check if user tenant1 can access tenant2
+kubectl get pods -n tenant2-apps --kubeconfig kubeconfig_tenant1.yaml
+
+# Cleanup
+vcluster delete tenant1
+vcluster delete tenant2
