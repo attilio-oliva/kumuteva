e43531f7667295e6edfafa7932c9e937d9363ac0
Attilio Oliva <attilio.oliva@protonmail.com>
2025-01-23 14:40:37 +0100

    chore: untrack target folder

--- target/debug/deps/multi_tenancy_verifier-8a46044a7f5a0b85.d
+++
@@ -1,5 +0,0 @@
-/home/attilio/Documents/multi-tenancy-framework/multi_tenancy_verifier/target/debug/deps/libmulti_tenancy_verifier-8a46044a7f5a0b85.rmeta: src/main.rs
-
-/home/attilio/Documents/multi-tenancy-framework/multi_tenancy_verifier/target/debug/deps/multi_tenancy_verifier-8a46044a7f5a0b85.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/multi_tenancy_verifier-b47968ade8478454.d
+++
@@ -1,5 +0,0 @@
-/home/attilio/Documents/multi-tenancy-framework/multi_tenancy_verifier/target/debug/deps/libmulti_tenancy_verifier-b47968ade8478454.rmeta: src/main.rs
-
-/home/attilio/Documents/multi-tenancy-framework/multi_tenancy_verifier/target/debug/deps/multi_tenancy_verifier-b47968ade8478454.d: src/main.rs
-
-src/main.rs:
