3544f506fd0222268ede5054d7850d5c23905ec3
SteGala <galaxx.ste@gmail.com>
2026-01-27 08:45:15 +0100

    Add test duration for main script

--- overhead_results/run_tests.sh
+++ overhead_results/run_tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 <tools> <n_tenant> <kubeconfig_path>"
+    echo "Usage: $0 <tools> <n_tenant> <kubeconfig_path> <duration>"
     echo "  tools: Comma separated list of tools to install [vcluster,capsule,kubevirt,reference]"
     echo "  n_tenant: Number of tenats to generate"
     echo "  kubeconfig_path: Path to kubeconfig file"
+    echo "  duration: duration of the load for each tenant"
     exit 1
 }
 
 # Check arguments
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
     usage
     exit 1
 fi
@@ -23,6 +24,7 @@ fi
 
 TOOLS="$1"
 N_TENANT="$2"
+DURATION="$4"
 
 IFS=',' read -ra TOOL_ARRAY <<< "$TOOLS"
 for TOOL in "${TOOL_ARRAY[@]}"; do
@@ -31,25 +33,25 @@ for TOOL in "${TOOL_ARRAY[@]}"; do
         vcluster)
             echo "Provisioning vCluster tenants"
             cd vcluster
-            ./run_test.sh create "$N_TENANT" "$KUBECONFIG_PATH"
+            ./run_test.sh create "$N_TENANT" "$KUBECONFIG_PATH" "$DURATION"
             cd ..
             ;;
         capsule)
             echo "Provisioning Capsule tenants"
             cd capsule
-            ./run_test.sh "$N_TENANT" "$KUBECONFIG_PATH"
+            ./run_test.sh "$N_TENANT" "$KUBECONFIG_PATH" "$DURATION"
             cd ..
             ;;
         kubevirt)
             echo "Provisioning KubeVirt tenants"
             cd kubevirt
-            ./run_test.sh "$N_TENANT" "$KUBECONFIG_PATH"
+            ./run_test.sh "$N_TENANT" "$KUBECONFIG_PATH" "$DURATION"
             cd ..
             ;;
         reference)
             echo "Provisioning reference tenants"
             cd reference
-            ./run_test.sh "$N_TENANT" "$KUBECONFIG_PATH"
+            ./run_test.sh "$N_TENANT" "$KUBECONFIG_PATH" "$DURATION"
             cd ..
             ;;
         *)
